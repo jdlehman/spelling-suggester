3174f930c6c4b3eb67818efcd5271e2fcb8fde82
Jonathan Lehman <jonathan.lehman91@gmail.com>
2013-09-05 22:36:43 -0400

    Clear old search and input text
    Add comments to speller.js

--- public/speller.js
+++ public/speller.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
   var socket = io.connect('/');
 
+  // send text-field to server
   $('#correct').click(function() {
-    var text = $('input').val();
+    var text = $('#input').val();
     socket.emit('spell-check', { word : text });
   });
 
+  // clear old suggestion and input
+  $('#input').click(function() {
+    $(this).val('');
+    $('#corrected').html('');
+  });
+
+  // Fill in corrected word from server
   socket.on('corrected', function(data) {
     $('#corrected').html(data.correct);
   });
